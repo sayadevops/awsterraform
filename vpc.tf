cfa6710a0d540bb1f90473de39582de68299e213
sayainfotech <abhisheksaya98@gmail.com>
2023-03-19 15:45:11 -0400

    vpc

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+#creation of vpc
+resource "aws_vpc" "lms" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "lms"
+  }
+}
